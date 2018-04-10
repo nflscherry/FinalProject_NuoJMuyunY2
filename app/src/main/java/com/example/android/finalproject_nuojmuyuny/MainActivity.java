3ce7a8164d2c62d594b0f3b11cd647b5d42c00ab
Nuo Jia <nj2368@tc.columbia.edu>
2018-04-09 21:00:39 -0400

    cherry change1

--- app/src/main/java/com/example/android/finalproject_nuojmuyuny/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject_nuojmuyuny/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.android.finalproject_nuojmuyuny;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.android.finalproject_nuojmuyuny.R.id.sign_up;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void signUp(View view){
+        signUp = findViewById(sign_up);
+        Intent newIntent;
+        newIntent = new Intent(this,InformationActivity.class);
+        startActivity(newIntent);
     }
 }
